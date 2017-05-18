373ad73b64c38d0123016ed4e60efa1040c81150
Wenzhao Liu <zmaxlwz@gmail.com>
2017-05-18 10:21:57 -0400

    update

--- code/geocoding.py
+++ code/geocoding.py
@@ -76,7 +76,7 @@ class GeoCoding:
             count += 1            
             #if count > 4:
             #    break
-            if count <= 9926:
+            if count <= 19927:
                 continue
             print(count)    
             asset_id = row[0]

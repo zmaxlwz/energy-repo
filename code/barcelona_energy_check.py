f304ffa9d4e90700887784867092ef0126c96bf2
Wenzhao Liu <zmaxlwz@gmail.com>
2017-06-02 14:55:39 -0400

    update

--- code/barcelona_energy_check.py
+++ code/barcelona_energy_check.py
@@ -295,9 +295,9 @@ class BarcelonaEnergyCheck:
         """               
         self.connect_db()
         #asset_id_list = [2063, 2, 3, 10, 11]
-        asset_id_list = [2063]
+        #asset_id_list = [2063]
         #asset_id_list = [2100, 2102, 2103, 2110, 2111, 2112]
-        #asset_id_list = self.get_assets_list()
+        asset_id_list = self.get_assets_list()
 
         start_time = datetime.datetime(2016, 11, 1, 0, 0, 0)
         end_time = datetime.datetime(2017, 4, 30, 0, 0, 0)

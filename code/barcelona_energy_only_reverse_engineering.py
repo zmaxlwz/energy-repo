0c170bf18d8d3590f065a29e2360aef916eb3249
Wenzhao Liu <zmaxlwz@gmail.com>
2017-06-15 13:42:59 -0400

    update

--- code/barcelona_energy_only_reverse_engineering.py
+++ code/barcelona_energy_only_reverse_engineering.py
@@ -350,7 +350,7 @@ class DayburnerEnergyOnly:
                 lastDate = currentDate
                 lastEnergy = currentEnergy 
 
-        print(energy_rolling_window)
+        #print(energy_rolling_window)
         #print(self.get_most_frequent_value(energy_rolling_window))
         #print(currentDate)
 
@@ -398,7 +398,7 @@ class DayburnerEnergyOnly:
             if len(energy_rolling_window) >= 30:
                 # the rolling window is full
                 break
-        print(energy_rolling_window)
+        #print(energy_rolling_window)
         energy_rolling_window = [x for x in energy_rolling_window if x != 0]
         if len(energy_rolling_window) == 0:
             return None      
@@ -470,7 +470,7 @@ class DayburnerEnergyOnly:
                 if actual_wattage is None:
                     continue
 
-                print(actual_wattage)    
+                #print(actual_wattage)    
 
                 #results = self.find_dayburners_energy_with_actual_wattage(component_id_tuple, actual_wattage, start_time, end_time)             
                 results = self.find_dayburners_aggregation_energy_with_actual_wattage(component_id_tuple, actual_wattage, start_time, end_time)                
@@ -528,8 +528,8 @@ class DayburnerEnergyOnly:
         self.computeSunTime(self.suntime_latitude, self.suntime_longitude, self.startDate, self.endDate)
         #step 3:  get components list
         #component_id_list = [951]
-        #component_id_list = self.getComponentsList()
-        component_id_list = [(2003, 3957, 41.3826797751266, 2.17683879808138, '2016-02-19', '2016-02-19', 'Placa de Sant Jaume', 3935, 72)]
+        component_id_list = self.getComponentsList()
+        #component_id_list = [(2003, 3957, 41.3826797751266, 2.17683879808138, '2016-02-19', '2016-02-19', 'Placa de Sant Jaume', 3935, 72)]
         #step 4:  call computeResults method
         self.computeResults(component_id_list)
         

e2162955c3b7d12219b556e4e3d1090a46fc980b
Wenzhao Liu <zmaxlwz@gmail.com>
2017-06-22 13:45:25 -0400

    update

--- code/check_open_faults.py
+++ code/check_open_faults.py
@@ -45,28 +45,7 @@ class OpenFaultsChecker:
         """    
         self.cur.close()
         self.conn.close()
-        
-    def process_input_file(self):
-        """ read each dayburner record from the input file 
-            and if there are open faults for the day-burner record, write to the output file
-
-        """
-        with open(self.inputFilename, "r") as csvFile:
-            csvReader = csv.reader(csvFile, delimiter=',') 
-            next(csvReader)
-            count = 0
-            for record in csvReader:
-                count += 1
-                #print(count)
-                asset_id = record[0]
-                component_id = record[1]
-                current_date = datetime.datetime.strptime(record[10], '%Y-%m-%d').date()       
-                num_faults = self.check_open_fault(asset_id, component_id, current_date)
-                if num_faults > 0:
-                	print(asset_id, component_id, current_date, num_faults)
-
-            print(count)    	
-
+            	
     def check_open_fault(self, asset_id, component_id, current_date):
         """ check if there are open faults during the day-burning date and time 
 
@@ -84,14 +63,68 @@ class OpenFaultsChecker:
 
         fault_rows = self.cur.fetchall()  
         
-        return len(fault_rows)   
+        results = []
+        for row in fault_rows:
+        	fault_id = row[0]
+        	error_key = row[1]
+        	first_reported_on = row[3]
+        	closed_on = row[9]
+        	results.append((fault_id, error_key, first_reported_on, closed_on))
+
+        #return len(fault_rows)   
+        return results
+
+    def process_input_file(self):
+        """ read each dayburner record from the input file 
+            and if there are open faults for the day-burner record, write to the output file
+
+        """
+        results = []
+        with open(self.inputFilename, "r") as csvFile:
+            csvReader = csv.reader(csvFile, delimiter=',') 
+            next(csvReader)
+            count = 0
+            for record in csvReader:
+                count += 1
+                #print(count)
+                asset_id = record[0]
+                component_id = record[1]
+                current_date = datetime.datetime.strptime(record[10], '%Y-%m-%d').date()
+                '''       
+                num_faults = self.check_open_fault(asset_id, component_id, current_date)
+                if num_faults > 0:
+                	print(asset_id, component_id, current_date, num_faults)
+                '''
+                open_faults = self.check_open_fault(asset_id, component_id, current_date)
+                for fault_record in open_faults:
+                	fault_id = fault_record[0]
+                	error_key = fault_record[1]
+                	first_reported_on = fault_record[2]
+                	closed_on = fault_record[3]
+                	results.append((asset_id, component_id, current_date, fault_id, error_key, first_reported_on, closed_on))
+
+            print(count)  
+
+        return results  
+
+    def write_to_file(self, results):
+        """ write results to output file
+
+        """
+        with open(self.outputFilename, "w") as csvFile:
+            csvWriter = csv.writer(csvFile, delimiter=',')   
+            title_row = ('asset_id', 'component_id', 'current_date', 'fault_id', 'error_key', 'first_reported_on', 'closed_on')         
+            csvWriter.writerow(title_row)
+            for record in results:
+                csvWriter.writerow(record)     
 
     def run(self):
         """ call this method to run the program
 
         """    
         self.connect_db()
-        self.process_input_file()
+        results = self.process_input_file()
+        self.write_to_file(results)
         self.disconnect_db()
 
 if __name__ == "__main__":
